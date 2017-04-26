a9d89af866b5bdaa22e618314ab26ee1a05cbd76
siddharth suthar <sid@siddharths-MacBook-Pro.local>
2017-04-26 16:06:09 -0700

    Change in the post reqest

--- server.js
+++ server.js
@@ -12,15 +12,15 @@ var mongoose   = require('mongoose');
 mongoose.connect('mongodb://54.183.242.172/test')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
- res.setHeader('Access-Control-Allow-Origin', '*');
- res.setHeader('Access-Control-Allow-Credentials', 'true');
- res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
- res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+// app.use(function(req, res, next) {
+//  res.setHeader('Access-Control-Allow-Origin', '*');
+//  res.setHeader('Access-Control-Allow-Credentials', 'true');
+//  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
- res.setHeader('Cache-Control', 'no-cache');
- next();
-});
+//  res.setHeader('Cache-Control', 'no-cache');
+//  next();
+// });
 
 var STATUS = "";
 
@@ -45,14 +45,14 @@ router.route('/order')
          order.size = req.body.size;
          order.name = req.body.name;
 
-
+ 
           order.save(function(err) {
             //order_id =order.id ;
             if (err){
                 STATUS = "Unsuccessful";
                 res.send(err);
             }
-            
+           console.log(req.body);
             STATUS = "Successful" ;
             res.status(200).send(order.id);
         
